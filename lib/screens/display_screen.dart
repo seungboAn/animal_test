6842fee50d6bc397663252cb59b7f7122b6d4687
seungboAn <seungbo1112@gmail.com>
2025-01-03 17:04:32 +0900

    feat: Add flutter dotenv

--- lib/screens/display_screen.dart
+++ lib/screens/display_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:camera/camera.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 // A widget that displays the picture taken by the user.
 class DisplayPictureScreen extends StatefulWidget {
@@ -58,57 +59,66 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
             child: Container(
               color: Colors.black,
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      foregroundColor: Colors.white,
-                      minimumSize: Size(200, 60),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
+                  Expanded(
+                      child: IconButton(
+                    icon: const Icon(Icons.arrow_back_ios_rounded,
+                        color: Colors.white),
+                    onPressed: () => Navigator.of(context).pop(),
+                  )),
+                  Expanded(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        minimumSize: Size(200, 60),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
                       ),
+                      onPressed: () async {
+                        setState(() {
+                          _isLoading = true;
+                        });
+
+                        // Gemini TEST
+                        final apiKey = dotenv.env['gemini_key'];
+                        if (apiKey == null) {
+                          print('No \$API_KEY environment variable');
+                          exit(1);
+                        }
+                        // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+                        final model = GenerativeModel(
+                            model: 'gemini-1.5-flash', apiKey: apiKey);
+                        final imageFile =
+                            await XFile(widget.imagePath).readAsBytes();
+                        final prompt = TextPart(_prompt);
+                        final imageParts = [
+                          DataPart('image/jpeg', imageFile),
+                        ];
+                        final response = await model.generateContent([
+                          Content.multi([prompt, ...imageParts])
+                        ]);
+
+                        final url =
+                            'https://image.pollinations.ai/prompt/japan-animated-cute-${response.text}';
+                        final imageResponse = await http.get(Uri.parse(url));
+                        setState(() {
+                          _imageResponse = imageResponse.bodyBytes;
+                          _response = response.text;
+                          _isLoading = false;
+                        });
+                      },
+                      child: _isLoading
+                          ? const CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation(Colors.white),
+                            )
+                          : Text(_response == '' ? '동물 찾기' : _response,
+                              style: const TextStyle(fontSize: 16)),
                     ),
-                    onPressed: () async {
-                      setState(() {
-                        _isLoading = true;
-                      });
-
-                      // Gemini TEST
-                      const apiKey = 'Enter_your_API_KEY';
-                      if (apiKey == null) {
-                        print('No \$API_KEY environment variable');
-                        exit(1);
-                      }
-                      // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
-                      final model = GenerativeModel(
-                          model: 'gemini-1.5-flash', apiKey: apiKey);
-                      final imageFile =
-                          await XFile(widget.imagePath).readAsBytes();
-                      final prompt = TextPart(_prompt);
-                      final imageParts = [
-                        DataPart('image/jpeg', imageFile),
-                      ];
-                      final response = await model.generateContent([
-                        Content.multi([prompt, ...imageParts])
-                      ]);
-
-                      final url =
-                          'https://image.pollinations.ai/prompt/japan-animated-cute-${response.text}';
-                      final imageResponse = await http.get(Uri.parse(url));
-                      setState(() {
-                        _imageResponse = imageResponse.bodyBytes;
-                        _response = response.text;
-                        _isLoading = false;
-                      });
-                    },
-                    child: _isLoading
-                        ? const CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation(Colors.white),
-                          )
-                        : Text(_response == '' ? '분석' : _response,
-                            style: const TextStyle(fontSize: 16)),
                   ),
+                  Expanded(child: Container()),
                 ],
               ),
             ),
